dea2029729800fa8c3ef72cb02ccc33c1e68dbec
Dianis Caro <dm.caro@uniandes.edu.co>
2021-10-01 14:28:26 -0500

    Final final

--- src/controller.js
+++ src/controller.js
@@ -7,18 +7,20 @@ const getPairsOfPlayers = async (req = request, resp = response, next) => {
     const {input} = req.query;
     const { data } = await axios('https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players');
     const results = data.values.map((elm) => ({first_name: elm.first_name, last_name: elm.last_name, height: elm.h_in}));
-    arrayPlayers = [];
+    const arrayPlayers = [];
+    console.log(input);
     for (let index = 0; index < results.length; index++) {
       const element = results[index];
-      for (let index2 = 0; index2 < results.length; index2++) {
-        const element2 = results[2];
-        if((parseInt(element.height) + parseInt(element2.height)) === input && arrayPlayers.length === 0)
+      for (let index2 = 1; index2 < results.length; index2++) {
+        const element2 = results[index2];
+        const sum = parseInt(element.height) + parseInt(element2.height);
+        if(sum === parseInt(input) && arrayPlayers.findIndex([element2,element]) === parseInt(-1))
         {
-          console.log(true);
+          arrayPlayers.push([element,element2]);
         }
       };
-    };
-    resp.json(arrayPlayers).send();
+    }; 
+    resp.json(arrayPlayers);
   } catch (error) {
     resp.status(500).json({ error });
   }
